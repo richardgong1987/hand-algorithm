a481c3ba3e1a28b16d44de2a93974c4d3e022b31
gong.hanjin <richardgong1988@gmail.com>
2022-12-24 02:35:24 +0900

    leetcde-336 part 2

--- 336. Palindrome Pairs.py
+++ 336. Palindrome Pairs.py
@@ -19,3 +19,39 @@ solution = Solution1()
 pairs = solution.palindromePairs(["abcd", "dcba", "lls", "s", "sssll"])
 print(pairs)
 
+
+# part 2
+class Solution2:
+    def is_palindrome(self, word):
+        return word == word[::-1]
+
+    def palindromePairs(self, words):
+        d = {}
+
+        for i, w in enumerate(words):
+            d[w] = i
+
+        result = []
+
+        for i, w in enumerate(words):
+            for j in range(len(w) + 1):
+
+                prefix, suffix = w[:j], w[j:]
+                prefix_r, suffix_r = prefix[::-1], suffix[::-1]
+                print(f'prefix="{prefix}",prefix_r="{prefix_r}"')
+                print(f'("{prefix}")("{suffix}")')
+                print(f'suffix="{suffix}", suffix_r="{suffix_r}"')
+
+
+                if self.is_palindrome(prefix) and suffix_r in d:
+                    if i != d[suffix_r]:
+                        print("A********: ok")
+                        result.append([d[suffix_r], i])
+
+                if self.is_palindrome(suffix) and suffix and prefix_r in d:
+                    if i != d[prefix_r]:
+                        print("B*******: ok")
+                        result.append([i, d[prefix_r]])
+                print("---------------------------")
+
+        return result
