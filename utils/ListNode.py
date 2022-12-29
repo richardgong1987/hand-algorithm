dde40fb8d22d454baa6868cc270df47398f9248a
gong.hanjin <richardgong1988@gmail.com>
2022-12-30 01:19:16 +0900

    update 899

--- utils/ListNode.py
+++ utils/ListNode.py
@@ -1,4 +1,27 @@
+from typing import List
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+def createListNode(ls: List[int]) -> ListNode:
+    p1 = ListNode()
+    p2 = p1
+    for _, val in enumerate(ls):
+        p2.next = ListNode(val)
+        p2 = p2.next
+
+    return p1.next
+
+
+def swap(a: ListNode, b: ListNode):
+    a.val, b.val = b.val, a.val
+
+
+def printListNode(ll:ListNode):
+    while ll:
+        print(ll.val, end='')
+        ll = ll.next
