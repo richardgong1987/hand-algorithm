f6edf350d3fe632a6d503c867f8f8b03e93b27f4
pc <pc@pc.com>
2023-05-26 23:14:34 +0900

    subarray beauty

--- programming-contest/3.Weekly Contest/6431. Neighboring Bitwise XOR.py
+++ programming-contest/3.Weekly Contest/6431. Neighboring Bitwise XOR.py
@@ -52,27 +52,26 @@ from typing import List
 
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        n = len(derived)
 
-        for bit in [0, 1]:
-            original = [bit]
+        """
+        Explanation
+        orginal: A[0], A[1], .... A[n-1]
+        derived: A[0]^A[1], A[1]^A[2] .... A[n-1]^A[0]
 
-            for i in range(1, n):
-                original.append(derived[i - 1] ^ original[i - 1])
+        xor(derived) = (A[0]^A[1])^(A[1]^A[2])^ .... ^(A[n-1]^A[0]) = 0
 
-            if all(derived[i] == (original[i] ^ original[(i + 1) % n]) for i in range(n)):
-                return True
+        The necessary and suffisant condition for derived to have an original is
+        xor(derived) == 0
 
-        return False
+        Whend original and derived is binary sequence,
+        this equals to sum(derived) % 2 == 0.
 
 
+        Complexity
+        Time O(n)
+        Space O(1)
 
-"""
-This problem can be solved by using a greedy algorithm.
+        """
+        return sum(derived) % 2 ==0
 
-The idea here is to iterate over the derived array and set the values of the original array. Given derived[i] = original[i] ⊕ original[i + 1], we can find original[i+1] if we know original[i] and derived[i]. The issue here is we don't initially know any value in the original array. However, we can make an assumption for original[0] and then construct the original array.
 
-Given derived[i] = original[i] ⊕ original[i + 1], if we rearrange this equation, we get original[i+1] = derived[i] ⊕ original[i].
-
-Since XOR of two same numbers is 0 and XOR of two different numbers is 1, we can assume original[0] = 0 and construct the rest of the array. If the constructed array satisfies the condition derived[i] = original[i] ⊕ original[(i + 1) % n] for every i in the range [0, n), then we return true. If not, we then assume original[0] = 1 and construct the array again. If the second assumption also doesn't work, we return false.
-"""
